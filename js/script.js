81a95003c16ff8920d9ac9d657340f2e3cf33810
DESKTOP-0V9VAH6\ashok <ashok.chakravarthi@zohocorp.com>
2021-12-18 19:04:27 +0530

    Dice roll logic implemented

--- js/script.js
+++ js/script.js
@@ -2,17 +2,32 @@
 // If no argument is passed, 6 is set, ie, returns a number between and including 1 to 6
 // 		For example: 
 //    getRandomDiceRoll() will return either 1, 2, 3, 4, 5, 6
+
+let diceRoll = 1;
 const getRandomDiceRoll = function(sides=6) {
-  return Math.floor( Math.random() * sides ) + 1
+  diceRoll = Math.floor( Math.random() * sides ) + 1
 }
 
 // Here's a test roll, check the console!
-console.log(getRandomDiceRoll())
+function rollTheDice(){
+  getRandomDiceRoll();
+  showImage();
+}
+
+const showImage = function() {
+  var img = document.createElement("img");
+  img.src = "./img/dice"+ diceRoll + ".svg";
+  document.querySelector("#dice").innerHTML = '<img src="'+img.src+'" height=150 width=150/>';
+  document.querySelector("#caption").textContent = "You Rolled: "+ diceRoll;
+  return null;
+}
 
 // 1) Call getRandomDiceRoll() and store the result as a variable named "diceRoll"
 
 // 2) Update user interface (document), showing the diceface (svg image) that matches the roll number
-
+const roleDiceButton = document.querySelector(`#roll_dice`)
+roleDiceButton.addEventListener(`click`, rollTheDice)
+window.addEventListener(`load`, rollTheDice)
 // 3) Use `diceRoll` to update the label "You rolled: #" (replacing # with the roll)
 
 // 4) Wrap the dice roll procedure in a function named rollTheDice(), call it from the console to test
